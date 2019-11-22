8a918039cfc14f8fee08e9f705cb2cbc0506a56e
dcmeglio <dmeglio@gmail.com>
2019-11-21 21:24:05 -0500

    Implemented button capabilities

--- drivers/BOND_Fan_Timer_Light.groovy
+++ drivers/BOND_Fan_Timer_Light.groovy
@@ -9,6 +9,9 @@ metadata {
     definition (name: "BOND Fan Timer Light", namespace: "bond", author: "dmeglio@gmail.com") {
 		capability "Switch"
         capability "Light"
+		capability "PushableButton"
+		capability "HoldableButton"
+		capability "ReleasableButton"
 		
 		command "dim", ["number"]
     }
@@ -24,4 +27,12 @@ def on() {
 
 def off() {
     parent.handleLightOff(device, device.deviceNetworkId.split(":")[1])
+}
+
+def installed() {
+	sendEvent(name: "numberOfButtons", value: "1")
+}
+
+def updated() {
+	sendEvent(name: "numberOfButtons", value: "1")
 }
